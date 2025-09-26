48e7256789bc0eb365b72747a120a53b180475e1
golla-sreenivas-yadav <Srinivaspa374@gmail.com>
2025-09-26 16:31:06 +0530

    index update

--- backend/index.ts
+++
@@ -1,5 +0,0 @@
-// The actual Express app lives one folder deeper (in src/index.ts).
-// This file just imports and exports it so Vercel loads the whole thing.
-import app from './src/index'; 
-
-export default app;
